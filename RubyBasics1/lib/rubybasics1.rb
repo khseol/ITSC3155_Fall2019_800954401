6e873c2495a1acde8275012ccfebbed1f6540503
Kathy Seol <kseol@uncc.edu>
2019-09-03 19:36:50 +0000

    RubyBasic1 -part3 V2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -19,13 +19,25 @@ end
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if (arr.length == 0)
+    return false
+  end
   if (arr.length == 1)
     return false
-  elsif(arr.empty? == true)
-    return false
-  else
-    #methods found and to use: combination, and any?
-    arr.combination(2).any? {|x,y| x+y == n}
   end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
+    end
   end
+  return false
+end
