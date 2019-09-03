598e5588ac35dc17a65db07fa621af3a4a2c7ca7
Kathy Seol <kseol@uncc.edu>
2019-09-03 15:05:50 +0000

    RubyBasics1 -Part1

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -2,13 +2,19 @@
 # Part I
 def sum arr
   # YOUR CODE HERE
-  
   return arr.sum
 end
 
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
+  if (arr.empty? == true)
+    return 0
+  elsif (arr.length == 1)
+    return arr[0]
+  else 
+    return arr.sort.last(2).sum
+  end  
 end
 
 # Part III
