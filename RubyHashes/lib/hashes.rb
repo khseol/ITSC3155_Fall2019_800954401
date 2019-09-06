33648e8f5da88b1f0248c773070ceabcbe51bea5
Kathy Seol <kseol@uncc.edu>
2019-09-06 02:27:35 +0000

    RubyHashes -Part1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -2,6 +2,18 @@
 # Part I
 def array_2_hash emails, contacts
     # YOUR CODE HERE
+    contactInfor = {}
+    index = 0
+    if emails [0] == nil
+        return contacts
+    else
+        contacts.each do |key, value|
+            tempoKey = "#{key}"
+            contactInfor[tempoKey] = emails[index]
+            index += 1
+        end 
+    end    
+       return contactInfor     
 end
 
 # Part II
