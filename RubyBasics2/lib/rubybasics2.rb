73410c1fe39a35099802d12d1caddb8712675847
Kathy Seol <kseol@uncc.edu>
2019-09-14 15:52:54 +0000

    Ruby Basics 2 -part2

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -7,14 +7,22 @@ def hello(name)
     message = 'Hello, ' + name
     
     return message
-   
-  
 end
 
 # Part II
-def starts_with_consonant? s
+def starts_with_consonant? s #parameter is a string
   # YOUR CODE HERE
+  #conditional to check is the first letter in the name contains a constant...
+  
+  if s[0] =~ /[^AEIOU]/ && s[0] =~ /[^aeiou]/ && s[0] =~ /[^\d]/ && s[0] =~ /[^$&+,:;=?@#]/
+    return true
+  else
+    return false
+  end  
+  
+  
 end
+
 
 # Part III
 def binary_multiple_of_4? s
