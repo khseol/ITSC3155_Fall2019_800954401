70d6b43260d568a136ff37084650c8f266dc3642
Kathy Seol <kseol@uncc.edu>
2019-09-21 21:38:54 +0000

    Ruby 00p -part1

--- ruby_calisthenics2/lib/dessert.rb
+++ ruby_calisthenics2/lib/dessert.rb
@@ -1,8 +1,18 @@
 class Dessert
-  # add code for setters and getters
+  # add code for setters and getters...so...attr_accessor?
+  
+  #from the reading...
+  #attribute is a property with named getter and/or setter methods
+  attr_accessor :name
+  attr_accessor :calories
+  
+  
   def initialize(name, calories)
     # your code here
+    @name = name
+    @calories = calories
   end
+  
   def healthy?
     # your code here
   end
@@ -17,3 +27,4 @@ class JellyBean < Dessert
     # your code here
   end
 end
+
