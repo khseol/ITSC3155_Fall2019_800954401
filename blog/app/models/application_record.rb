a0548fe624860bdd88b49ce3e7bfe8d72947ae8c
Kathy Seol <kseol@uncc.edu>
2019-10-17 18:47:26 +0000

    fixed the validation and message for title

--- blog/app/models/application_record.rb
+++ blog/app/models/application_record.rb
@@ -1,5 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 5 }
-  #pressence: true -- Title cannot be empty
-  #length {minimum: 5} -- Title must be AT LEAST 5 characters long
+  self.abstract_class = true
 end
