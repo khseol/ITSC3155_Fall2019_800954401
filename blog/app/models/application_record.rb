ac0db605ba37233c779ae93ddc1444e6a1a50ab7
Kathy Seol <kseol@uncc.edu>
2019-10-17 16:31:21 +0000

    Add valiation for the title

--- blog/app/models/application_record.rb
+++ blog/app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+  validates :title, presence: true, length: { minimum: 5 }
+  #pressence: true -- Title cannot be empty
+  #length {minimum: 5} -- Title must be AT LEAST 5 characters long
 end
