cb519d25c27b58d7f7c416c0b6c116db10d8fcfb
Kathy Seol <kseol@uncc.edu>
2019-10-27 19:53:28 +0000

    Add form partial and update New view

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -13,10 +13,10 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        byebug
+        #byebug
         
         if @article.save
-            byebug
+            #byebug
             redirect_to @article
         else
             render 'new'
