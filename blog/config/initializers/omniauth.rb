84465e4eda310c1b2c693fce083c3846703404e3
Kathy Seol <kseol@uncc.edu>
2019-11-12 04:35:28 +0000

    Finalize google sign in functionality - unfinished

--- blog/config/initializers/omniauth.rb
+++ blog/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '374087811995-cajd2mefpte5lni23527ei988nf21get.apps.googleusercontent.com', 'bIbnqC6-O5pOovuyog9ZehRu', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '528712181092-rsilqpjboqaj398kjkk957c8guji45rv.apps.googleusercontent.com', 'zfB_MLkU-Y_bSekUhjAPPdjw', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
+  resources :sessions, only: [:create, :destroy]
   get 'welcome/index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
